7378b03b0a6bfbcdad5054e31b6fd2e61ac18b4d
anuppoudel54 <anuppoudel54@gmail.com>
2021-10-10 17:32:06 +0545

    Update nginx-entrypoint.sh

--- alpinetest/nginx_service/nginx-entrypoint.sh
+++ alpinetest/nginx_service/nginx-entrypoint.sh
@@ -3,14 +3,14 @@ echo " installing ssl"
 #file = "/etc/letsencrypt/live/test.anuppoudel.codes/fullchain.pem"
 
 #if [[ -f "$file" ]];
-#if cat /etc/letsencrypt/live/test.anuppoudel.codes/fullchain.pem 
-#then
+if cat /etc/letsencrypt/live/test.anuppoudel.codes/fullchain.pem 
+then
 
-#echo "certs exist"
-#else
-#echo "cerst not installed, installing"
+echo "certs exist"
+else
+echo "certs not installed, installing"
 certbot --nginx -m anuppoudel45@gmail.com --agree-tos -d serve.anuppoudel.codes --redirect --non-interactive
 
 echo "DONE"
-#fi
+fi
 exec "$@"
