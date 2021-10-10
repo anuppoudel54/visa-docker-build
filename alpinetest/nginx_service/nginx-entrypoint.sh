0495856f0a870f43321f1e253094e711df63400c
anuppoudel54 <anuppoudel54@gmail.com>
2021-10-10 16:51:00 +0545

    Update nginx-entrypoint.sh

--- alpinetest/nginx_service/nginx-entrypoint.sh
+++ alpinetest/nginx_service/nginx-entrypoint.sh
@@ -9,7 +9,7 @@ echo " installing ssl"
 #echo "certs exist"
 #else
 #echo "cerst not installed, installing"
-#certbot --nginx -m anuppoudel45@gmail.com --agree-tos -d test.devk8.cf --redirect --non-interactive
+certbot --nginx -m anuppoudel45@gmail.com --agree-tos -d test.devk8.cf --redirect --non-interactive
 
 echo "DONE"
 #fi
