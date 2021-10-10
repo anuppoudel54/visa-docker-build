6429103fbbe6a2d79bf792dc985912c9c9d55c59
anuppoudel54 <anuppoudel54@gmail.com>
2021-10-10 23:12:41 +0545

    Update nginx-entrypoint.sh

--- alpinetest/nginx_service/nginx-entrypoint.sh
+++ alpinetest/nginx_service/nginx-entrypoint.sh
@@ -5,8 +5,9 @@ echo " installing ssl"
 #if [[ -f "$file" ]];
 if cat /etc/letsencrypt/live/serve.anuppoudel.codes/fullchain.pem 
 then
+echo "certs exist, attempting to renew"
+certbot renew --non-interactive
 
-echo "certs exist"
 else
 echo "certs not installed, installing"
 certbot --nginx -m anuppoudel45@gmail.com --agree-tos -d serve.anuppoudel.codes --redirect --non-interactive
